2c50956e3a9d85f453c1f4e58f8053b0dcb2581f
codeiwan <155705875+codeiwan@users.noreply.github.com>
2024-01-05 15:19:34 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,7 +1,6 @@
 def solution(a, b):
-    answer = day[(month[a] + b) % 7]
+    answer = day[(sum(month[:a]) + b) % 7]
     return answer
 
-month = {1: 0, 2: 31, 3: 60, 4: 91, 5: 121, 6: 152,
-         7: 182, 8: 213, 9: 244, 10: 274, 11: 305, 12: 335}
+month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30]
 day = ['THU','FRI','SAT','SUN','MON','TUE','WED']
