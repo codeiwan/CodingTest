e219cea0f18c81052d46ad5f9eb58d4e380e5d07
codeiwan <155705875+codeiwan@users.noreply.github.com>
2024-01-15 21:51:21 +0900

    [Silver IV] Title: 스택 2, Time: 792 ms, Memory: 131768 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,38 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class Main {
 
-    static LinkedList<Integer> stack = new LinkedList<>();
-
+    static Stack<Integer> stack = new Stack<>();
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
 
-        while(N --> 0){
-            st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-            if (command.equals("1")) {
-                stack.addFirst(Integer.parseInt(st.nextToken()));
-            } else if (command.equals("2")) {
-                sb.append(stack.isEmpty() ? -1 : stack.pollFirst()).append("\n");
-            } else if (command.equals("3")) {
-                sb.append(stack.size()).append("\n");
-            } else if (command.equals("4")) {
-                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
-            } else if (command.equals("5")) {
-                sb.append(stack.isEmpty() ? -1 : stack.getFirst()).append("\n");
-            }
+        while (N-- > 0) {
+            solution(br.readLine());
         }
+
         br.close();
 
         System.out.println(sb);
+    }
+
+    static void solution(String query) {
+        char c = query.charAt(0);
+
+        switch (c) {
+            case '1':
+                stack.push(Integer.parseInt(query.substring(2)));
+                return;
+            case '2':
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                return;
+            case '3':
+                sb.append(stack.size()).append("\n");
+                return;
+            case '4':
+                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                return;
+            case '5':
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                return;
+            default:
+                break;
+        }
     }
 }
