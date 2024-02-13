58ea97439e52bd64221e6f51ba02a2377d0b3b96
codeiwan <155705875+codeiwan@users.noreply.github.com>
2024-02-13 21:18:24 +0900

    [Silver II] Title: 이동하기, Time: 760 ms, Memory: 79496 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -4,15 +4,9 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[-1] * M for _ in range(N)]
-dp[0][0] = arr[0][0]
+DP = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        DP[i][j] = max(DP[i-1][j], DP[i][j-1]) + arr[i-1][j-1]
 
-for r in range(N):
-    for c in range(M):
-        if r < N-1:
-            dp[r+1][c] = max(dp[r+1][c], dp[r][c] + arr[r+1][c])
-        if c < M-1:
-            dp[r][c+1] = max(dp[r][c+1], dp[r][c] + arr[r][c+1])
-
-
-print(dp[N-1][M-1])
+print(DP[N][M])
