3f97f3259d2f9d90dc0afce9e11eb73f46bcfa91
codeiwan <155705875+codeiwan@users.noreply.github.com>
2024-03-17 18:24:33 +0900

    [Silver IV] Title: 암기왕, Time: 5344 ms, Memory: 185824 KB -BaekjoonHub

--- Python/백준/Silver/2776. 암기왕/암기왕.py
+++ Python/백준/Silver/2776. 암기왕/암기왕.py
@@ -1,22 +1,25 @@
-def find(num):
-    start, end = 0, N-1
-    while start <= end:
-        mid = (start + end) // 2
-        if note1[mid] == num:
-            return 1
-        elif note1[mid] < num:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return 0
+import sys
+input = sys.stdin.readline
 
+def main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        note1 = sorted(list(map(int, input().split())))
+        M = int(input())
+        note2 = list(map(int, input().split()))
+        for num in note2:
+            start, end = 0, N - 1
+            while start <= end:
+                mid = (start + end) // 2
+                if note1[mid] == num:
+                    print(1)
+                    break
+                elif note1[mid] < num:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            else:
+                print(0)
 
-T = int(input())
-
-for _ in range(T):
-    N = int(input())
-    note1 = sorted(list(map(int, input().split())))
-    M = int(input())
-    note2 = list(map(int, input().split()))
-    for num in note2:
-        print(find(num))
+main()
