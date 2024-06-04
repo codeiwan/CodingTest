b777bcc4502cf2fa3ba49116104fe3d194c3ef8d
codeiwan <155705875+codeiwan@users.noreply.github.com>
2024-06-04 19:48:12 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 1392 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
+++ Python/백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,16 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
-lst = list(map(int, input().split()))
-dp = [0] * N
+def main():
+    N = int(input().strip())
+    lst = list(map(int, input().split()))
+    dp = [0] * N
 
-for i in range(N-1, -1, -1):
-    cnt = 0
-    for j in range(N):
-        if cnt < dp[j]:
-            cnt = dp[j]
-        elif lst[i] == lst[j]:
-            dp[j] = cnt + 1
+    for i in range(N-1, -1, -1):
+        cnt = 0
+        for j in range(N):
+            if cnt < dp[j]:
+                cnt = dp[j]
+            elif lst[i] == lst[j]:
+                dp[j] = cnt + 1
 
-print(N - max(dp))
+    print(N - max(dp))
+    
+main()
